719c3b7d32b0663208da6252778239b0c4e5bd68
“emilesherrott” <emilesherrott@gmail.com>
2025-03-28 10:09:14 +0000

    Refactor findLargest

--- functions.js
+++ functions.js
@@ -1,4 +1,20 @@
-const findLargest = () => 1
+const findLargest = (arr) => {
+
+    if(!arr || arr.length == 0) {
+        return -1
+    }
+
+    let largest = arr[0]
+    let largestIndex = 0
+
+    for(let i=1; i<arr.length; i++){
+        if(arr[i] > largest){
+            largest = arr[i]
+            largestIndex = i
+        }
+    }
+    return largestIndex
+}
 
 module.exports = {
     findLargest
